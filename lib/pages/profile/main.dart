962df80e2d63b10590ba21d1b7105d69ec1f9b03
fkrdwii@gmail.com <fkrdwii@gmail.com>
2023-11-05 00:16:47 +0700

    profile screen dan edit profile

--- lib/pages/profile/main.dart
+++ lib/pages/profile/main.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
 
-class ProfilePage extends StatelessWidget {
-  const ProfilePage({super.key});
+import 'package:pambe_ac_ifa/pages/profile/screens/profile_screens.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+        primarySwatch: Colors.amber,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return ProfileScreen();
   }
 }
